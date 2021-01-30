3936e1b50d0b9da22c279880d001e4db119a1f4b
usagifm <44514120+usagifm@users.noreply.github.com>
2021-01-30 22:10:25 +0700

    update target static

---
+++ dist/_nuxt/static/1612019401/login/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1612019401",layout:"default",error:a,state:{isAuth:b,access_token:void 0,loading:a,class:{classes:[],classDetail:[],classMeetings:[],meetingDetail:[],lessonDetail:[]},profile:{},auth:{user:a,loggedIn:b,strategy:"local"}},serverRendered:true,routePath:"\u002Flogin",config:{}}}(null,false));
