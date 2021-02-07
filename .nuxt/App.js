24955cdc0ad197ce767ef15d70e21ecfe9281d21
usagifm <44514120+usagifm@users.noreply.github.com>
2021-02-07 18:10:19 +0700

    capek

--- .nuxt/App.js
+++ .nuxt/App.js
@@ -3,6 +3,7 @@ import Vue from 'vue'
 import { getMatchedComponentsInstances, getChildrenComponentInstancesUsingFetch, promisify, globalHandleError, urlJoin, sanitizeComponent } from './utils'
 import NuxtError from './components/nuxt-error.vue'
 import NuxtLoading from './components/nuxt-loading.vue'
+import NuxtBuildIndicator from './components/nuxt-build-indicator'
 
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 
@@ -29,6 +30,8 @@ import '../assets/css/wave.css'
 import '../node_modules/vuesax/dist/vuesax.css'
 
 import '../node_modules/material-design-icons/iconfont/material-icons.css'
+
+import '../node_modules/vuetify/dist/vuetify.css'
 
 import _6b4fb138 from '../layouts/backup.vue'
 import _6f6c098b from '../layouts/default.vue'
@@ -71,7 +74,7 @@ export default {
       }
     }, [
       loadingEl,
-
+      h(NuxtBuildIndicator),
       transitionEl
     ])
   },
@@ -122,10 +125,6 @@ export default {
 
     isFetching () {
       return this.nbFetching > 0
-    },
-
-    isPreview () {
-      return Boolean(this.$options.previewData)
     },
   },
 
@@ -211,6 +210,10 @@ export default {
     },
 
     setLayout (layout) {
+      if(layout && typeof layout !== 'string') {
+        throw new Error('[nuxt] Avoid using non-string value as layout property.')
+      }
+
       if (!layout || !layouts['_' + layout]) {
         layout = 'default'
       }
